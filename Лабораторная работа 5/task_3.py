07713fbc51b7a0da48bfcc97d3b69c779e50b301
Боташева Л.Б <115580268+LarisaBotasheva@users.noreply.github.com>
2022-11-15 00:48:54 +0300

    Update task_3.py

--- Лабораторная работа 5/task_3.py
+++ Лабораторная работа 5/task_3.py
@@ -3,7 +3,7 @@ from random import sample
 def get_unique_list_numbers() -> list[int]:
     list_numbers = []
     list_len = 15
-    i = random.sample(range(-10, 11), 15)
+    i = random.sample(range(-10, 11), list_len)
     list_numbers.append(i)
     return list_numbers
 print(get_unique_list_numbers())
